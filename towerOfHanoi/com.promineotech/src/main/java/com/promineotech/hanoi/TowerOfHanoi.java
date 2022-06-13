b5c36963a953c3c109457ddd963532d5bc77468f
Matt Sagar <mattsagar0@gmail.com>
2022-06-12 20:00:00 -0500

    first commit

---
+++ towerOfHanoi/com.promineotech/src/main/java/com/promineotech/hanoi/TowerOfHanoi.java
@@ -0,0 +1,13 @@
+package com.promineotech.hanoi;
+
+@SpringBootApplication 
+public class TowerOfHanoi {
+private static final int MIN_Height = 1;
+private static final int MAX_Height = 10;
+private static final int DEF_HEIGHT = 3;
+
+public static void main(String[] args) {
+	
+}
+
+}
